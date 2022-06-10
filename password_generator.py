a22c31841ccfd8f85ee9943f985de3156075af7f
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-06-10 23:33:43 +0100

    initial commit

---
+++ password_generator.py
@@ -0,0 +1,19 @@
+import random
+
+print('Welcome to the Password Generator!')
+
+characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"£$%^&*()_+'
+
+number = input('\nHow many passwords would you like to generate? ')
+number = int(number)
+
+length = input('\nHow many characters would you like your password to have? ')
+length = int(length)
+
+print('\nHere you go!')
+
+for pwd in range(number):
+    passwords = ''
+    for char in range(length):
+        passwords += random.choice(characters)
+    print(passwords)
